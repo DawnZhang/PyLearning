c78d621eb44fe41530cd729c21849ace14a0210a
DawnZhang <xuzhizhang@umass.edu>
2016-03-17 23:56:58 -0400

    Substitute raw_input() for input()

--- Practices/backaccount.py
+++ Practices/backaccount.py
@@ -1,27 +1,27 @@
 class BankAccount(object):
-	balance = 0
-	def __init__(self, name):
-		self.name = name
-	def __repr__(self):
-		return "%s's account. Balance: $%.2f" % (self.name, self.balance)
-	def show_balance(self):
-		print("Balance: $%.2f" % self.balance)
-	def deposit(self, amount):
-		if amount <= 0:
-			print("Invalid deposit amount!")
-			return
-		else:
-			print("Deposit amount: $%.2f" % amount)
-			self.balance += amount
-			self.show_balance()
-	def withdraw(self, amount):
-		if amount > self.balance:
-			print("Invalid withdraw amount!")
-			return
-		else:
-			print("Withdraw amount: $%.2f" % amount)
-			self.balance -= amount
-			self.show_balance()
+    balance = 0
+    def __init__(self, name):
+	self.name = name
+    def __repr__(self):
+	return "%s's account. Balance: $%.2f" % (self.name, self.balance)
+    def show_balance(self):
+	print("Balance: $%.2f" % self.balance)
+    def deposit(self, amount):
+	if amount <= 0:
+	    print("Invalid deposit amount!")
+	    return
+	else:
+	    print("Deposit amount: $%.2f" % amount)
+	    self.balance += amount
+            self.show_balance()
+    def withdraw(self, amount):
+    	if amount > self.balance:
+            print("Invalid withdraw amount!")
+	    return
+	else:
+	    print("Withdraw amount: $%.2f" % amount)
+	    self.balance -= amount
+	    self.show_balance()
       
 my_account = BankAccount("Dawn")
 print(my_account)
